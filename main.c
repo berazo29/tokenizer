730d246cc8a609c37b7a3c293064de08a29f93fc
Bryan Erazo <br@MacBook-Pro.home>
2020-09-30 18:23:39 -0400

    Allocate memory and symbols func checks

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main( int argc, char *argv[argc+1]) {
 
@@ -10,5 +11,18 @@ int main( int argc, char *argv[argc+1]) {
         return EXIT_SUCCESS;
     }
 
+    // Allocate memory and init
+    unsigned int size = strlen(argv[1]);
+    char *str = (char *)malloc(size*sizeof(char));
+    strcpy(str, argv[1]);
+//    printf("%s", str);
+    for (int i = 0; i < size; ++i) {
+        if ( ispunct(str[i]) ){
+            char temp = str[i];
+            // Run the func for symbols
+        }
+
+
+    }
     return EXIT_SUCCESS;
 }
