5de56e0820ea42850ca6f82374e6371ee0af8ab2
Bryan Erazo <br@MacBook-Pro.home>
2020-09-30 16:56:39 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char *argv[argc+1]) {
+
+    //File name from arguments
+    if (argc != 2 ){
+        printf("Error\n");
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_SUCCESS;
+}
